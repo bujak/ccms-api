89b90dbfd61febe83104a4031f1c2a514ebf2c21
Tomasz Bujakowski <bujakowski@gmail.com>
2017-07-12 11:50:55 +0200

    Add request method for index in StudentController

--- src/main/java/com/codecool/ccms/api/student/StudentController.java
+++ src/main/java/com/codecool/ccms/api/student/StudentController.java
@@ -1,13 +1,14 @@
 package com.codecool.ccms.api.student;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/students")
 public class StudentController {
 
-    @RequestMapping("")
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public String index() {
         return "helol";
     }
