a07cca61adff3c97804879efbb7b1074b6f92f7b
Tomasz Bujakowski <bujakowski@gmail.com>
2017-07-12 13:46:22 +0200

    Add show in StudentController

--- src/main/java/com/codecool/ccms/api/student/StudentController.java
+++ src/main/java/com/codecool/ccms/api/student/StudentController.java
@@ -1,10 +1,7 @@
 package com.codecool.ccms.api.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +21,11 @@ public class StudentController {
     public Student add(@RequestBody Student student) {
         studentService.add(student);
         return student;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Student show(@PathVariable String id) {
+        return studentService.getById(id);
     }
 
 }
