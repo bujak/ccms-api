f51d23b25b5b45208b18b0941a16cb0b78488370
Tomasz Bujakowski <bujakowski@gmail.com>
2017-07-12 11:49:43 +0200

    Add StudentRepository

---
+++ src/main/java/com/codecool/ccms/api/student/StudentRepository.java
@@ -0,0 +1,12 @@
+package com.codecool.ccms.api.student;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by bujak on 12.07.17.
+ */
+
+@Repository
+public interface StudentRepository extends CrudRepository<Student, String> {
+}
