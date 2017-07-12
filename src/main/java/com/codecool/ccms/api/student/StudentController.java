4d58a0258bc40954d8a0fc7aac1d1090a0c30405
Tomasz Bujakowski <bujakowski@gmail.com>
2017-07-12 12:10:16 +0200

    Add getAll in index in StudentController

--- src/main/java/com/codecool/ccms/api/student/StudentController.java
+++ src/main/java/com/codecool/ccms/api/student/StudentController.java
@@ -1,15 +1,21 @@
 package com.codecool.ccms.api.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/students")
 public class StudentController {
 
+    @Autowired
+    private StudentService studentService;
+
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public String index() {
-        return "helol";
+    public List<Student> index() {
+        return studentService.getAll();
     }
 }
