8d1ccc5ee1bb1930813d17462a65f04494b50033
Tomasz Bujakowski <bujakowski@gmail.com>
2017-07-12 12:09:42 +0200

    Add getAll in StudentService

--- src/main/java/com/codecool/ccms/api/student/StudentService.java
+++ src/main/java/com/codecool/ccms/api/student/StudentService.java
@@ -3,6 +3,9 @@ package com.codecool.ccms.api.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by bujak on 12.07.17.
  */
@@ -10,5 +13,11 @@ import org.springframework.stereotype.Service;
 public class StudentService {
 
     @Autowired
-    StudentRepository studentRepository;
+    private StudentRepository studentRepository;
+
+    public List<Student> getAll() {
+        List<Student> students = new ArrayList<>();
+        studentRepository.findAll().forEach(students::add);
+        return students;
+    }
 }
