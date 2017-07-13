39adcfc0d9ef8a5fd49bcfdf69c9ce8a589ccf2e
Tomasz Bujakowski <bujakowski@gmail.com>
2017-07-13 14:33:13 +0200

    Add message source method in ApiApplication

--- src/main/java/com/codecool/ccms/api/ApiApplication.java
+++ src/main/java/com/codecool/ccms/api/ApiApplication.java
@@ -2,11 +2,23 @@ package com.codecool.ccms.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
 public class ApiApplication {
 
+	@Bean(name = "messageSource")
+	public ReloadableResourceBundleMessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageBundle = new ReloadableResourceBundleMessageSource();
+		messageBundle.setBasename("classpath:messages/messages");
+		messageBundle.setDefaultEncoding("UTF-8");
+		return messageBundle;
+	}
+
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(ApiApplication.class, args);
 	}
 }
